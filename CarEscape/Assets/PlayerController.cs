a2ab1ab6f1c16859155b99108fb011eb9c84245e
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-07-26 15:37:58 +0900

    staging games

---
+++ CarEscape/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start () {}
+
+	void Update () {
+
+        // 左矢印が押された時
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+        transform.Translate(-3, 0, 0);      // 左に「3」動かす
+
+        }
+
+        // 右矢印が押された時
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            transform.Translate(3, 0, 0);  // 右に「3」動かす
+            
+        }
+    }
+}
---
+++ Rooulette/Assets/RouletteController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RouletteController : MonoBehaviour {
+    float rotSpeed = 0; // 回転速度
+	void Start () {
+	}
+	void Update () {
+        // マウスが押されたら回転速度を設定する
+        if(Input.GetMouseButtonDown(0)) {
+            this.rotSpeed = 10;
+        }
+        // 回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotSpeed);
+        // ルーレットを減速させる(追加)
+        this.rotSpeed *= 0.96f;
+    }
+}
