c8e5438971e984b5eb89affe5a807fbd9b3d753f
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-07-27 03:16:42 +0900

    first commit of report

---
+++ shooting/Assets/ArrowController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(-6000,0,0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Shoot(Vector3 dir) {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    public void OnCollisionEnter(Collision other) {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
---
+++ shooting/Assets/ArrowGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject arrow = Instantiate(arrowPrefab) as GameObject;
+        arrow.GetComponent<ArrowController>().Shoot(new Vector3(0,200,2000));
+    }
+}
