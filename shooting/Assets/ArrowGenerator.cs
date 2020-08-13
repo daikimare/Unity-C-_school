9a6e4bcc760797327b58f8fa4844a175a6a3d8ae
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-08-14 02:55:49 +0900

    あげ直し

--- shooting/Assets/ArrowGenerator.cs
+++ shooting/Assets/ArrowGenerator.cs
@@ -6,10 +6,6 @@ public class ArrowGenerator : MonoBehaviour
 {
     public GameObject ArrowPrefab;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
